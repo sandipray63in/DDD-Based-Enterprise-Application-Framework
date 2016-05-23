e45b4f402ff6fcd0f23abb850e784450353e34b0
sandipray63in <sandip_ray63in@yahoo.com>
2016-05-24 00:51:29 +0530

    Checking in some initial files related to controller dispatchment

---
+++ Infrastructure/Utilities/SerializationUtility.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Infrastructure.Utilities
+{
+    public static class SerializationUtility
+    {
+        public static void Serialize<TSerializable>(string filePath)
+        {
+            var serializer = new XmlSerializer(typeof(TSerializable));
+            var writer = new StreamWriter(filePath);
+            serializer.Serialize(writer, default(TSerializable));
+            writer.Close();
+        }
+
+        public static TSerializable Deserialize<TSerializable>(string filePath)
+            where TSerializable : class
+        {
+            var serializer = new XmlSerializer(typeof(TSerializable));
+            var fileStream = new FileStream(filePath, FileMode.Open);
+            return serializer.Deserialize(fileStream) as TSerializable;
+        }
+    }
+}
---
+++ RestfulWebAPI/ControllerDispatchment/URIAndHeaderBasedHttpControllerSelector.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using System.Net.Http.Headers;
+using Infrastructure.Utilities;
+
+namespace RestfulWebAPI.ControllerDispatchment
+{
+    public class URIAndHeaderBasedHttpControllerSelector : DefaultHttpControllerSelector
+    {
+        private const string ControllerKey = "controller";
+
+        public URIAndHeaderBasedHttpControllerSelector(HttpConfiguration configuration) : base(configuration)
+        {
+
+        }
+
+        public override string GetControllerName(HttpRequestMessage request)
+        {
+            ContractUtility.Requires<ArgumentNullException>(request.IsNotNull(), "request cannot be null");
+            var routeData = request.GetRouteData();
+            // Get the controller version from header or url.
+            var controllerVersionInRequestObject = GetControllerVersion(request.Headers) ?? GetControllerVersion(request.RequestUri) ?? 0;
+            // Look up controller in route data
+            object controllerName = null;
+            routeData.Values.TryGetValue(ControllerKey, out controllerName);
+            var controllerNameInRequestObject = controllerName as string;
+            return controllerVersionInRequestObject == 0 ? controllerNameInRequestObject : string.Empty;
+        }
+
+        #region Private Methods
+
+        private double? GetControllerVersion(HttpRequestHeaders headers)
+        {
+            if (!headers.Contains("x-api-controller-version"))
+            {
+                return null;
+            }
+
+            var versionNumber = 0d;
+            var versionHeader = headers.GetValues("x-api-controller-version").FirstOrDefault();
+            if (versionHeader.IsNotNull())
+            {
+                double.TryParse(versionHeader, out versionNumber);
+            }
+            return versionNumber;
+        }
+
+        private double? GetControllerVersion(Uri requestUri)
+        {
+            if (requestUri.Query.IsNullOrEmpty())
+            {
+                return null;
+            }
+            var query = HttpUtility.ParseQueryString(requestUri.Query);
+            var versionNumber = 0d;
+            var version = query["controllerVersion"];
+            double.TryParse(version, out versionNumber);
+            return versionNumber;
+        }
+
+        #endregion
+    }
+}
