895f1b4a8a0e33ab662c85ea444b37ae5973b004
sandipray63in <sandip_ray63in@yahoo.com>
2016-07-09 11:16:27 +0530

    Update ContractUtility.cs

--- Infrastructure/Utilities/ContractUtility.cs
+++ Infrastructure/Utilities/ContractUtility.cs
@@ -3,9 +3,9 @@
 namespace Infrastructure.Utilities
 {
     /// <summary>
-    /// Although .NET Framework's ContractUtility.Requires should suffice in most of the cases 
+    /// Although .NET Framework's Contract.Requires should suffice in most of the cases 
     /// but it has the limitations that it cannot be applied on Class variables/properties and
-    /// method local variables(can only be applied on method parameters).Also all ContractUtility.Requires 
+    /// method local variables(can only be applied on method parameters).Also all Contract.Requires 
     /// code should be coded first and then any other code should be there else exception will 
     /// be thrown.To overcome these limitations, having this utility class
     /// 
@@ -17,8 +17,8 @@ namespace Infrastructure.Utilities
     /// wherein you don't have Visual Studio 2015 Enterprise Edition(which is required if you want
     /// to run the application manually or using unit test cases, in order to make it work for 
     /// Contract.Requires which comes under System.Diagnostics.Contracts namespace available within
-    /// the Contracts.DLL).But ideally, you should use Contract.Requires wherever possibl, if you 
-    /// are build some Enterprise app for production environment using Visual Studio 2015 
+    /// the Contracts.DLL).But ideally, you should use Contract.Requires wherever possible, if you 
+    /// build some Enterprise app for production environment using Visual Studio 2015 
     /// Enterprise Edition
     /// 
     /// </summary>
