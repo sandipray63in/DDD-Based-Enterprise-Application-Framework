ea8110dfa3a3731eca6043faf8e84f57d4634737
Sandip Ray <sandip_ray63in@yahoo.com>
2016-09-15 12:02:54 +0530

    Added an overloaded method in ContractUtility class.

--- Infrastructure/Utilities/ContractUtility.cs
+++ Infrastructure/Utilities/ContractUtility.cs
@@ -24,5 +24,21 @@ namespace Infrastructure.Utilities
                 throw Activator.CreateInstance(typeof(TException), exceptionMessage) as TException;
             }
         }
+
+        /// <summary>
+        /// This is useful when exceptionMessage itself needs to be build after checking the value of conditionToBeSatisfied.
+        /// </summary>
+        /// <typeparam name="TException"></typeparam>
+        /// <param name="conditionToBeSatisfied"></param>
+        /// <param name="exceptionMessageFunc"></param>
+        public static void Requires<TException>(bool conditionToBeSatisfied, Func<string> exceptionMessageFunc)
+            where TException : Exception
+        {
+            if (!conditionToBeSatisfied)
+            {
+                var exceptionMessage = exceptionMessageFunc();
+                throw Activator.CreateInstance(typeof(TException), exceptionMessage) as TException;
+            }
+        }
     }
 }
